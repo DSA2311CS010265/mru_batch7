c83e08389d99b52c7c19b3eb17731e38333c03f3
Chandhan Nehall <2311cs010265@mallreddyuniversity.ac.in>
2025-01-09 11:31:50 +0530

    I have made some changes

--- src/com/mru/day7/Demo.java
+++ src/com/mru/day7/Demo.java
@@ -3,8 +3,8 @@ package com.mru.day7;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("CSEBatch7");
-      	System.out.println("CSEBatch7");
+		System.out.println("dev1");
+      	
 	}
 
 }
